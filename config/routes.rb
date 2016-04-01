fa7f60cb37fba671938d6b8046a7282d8145c634
Erwol <ernes.wo@gmail.com>
2016-04-01 12:12:30 +0200

    Añadidas rutas, controlador y formulario de inserción del proveedor

--- config/routes.rb
+++ config/routes.rb
@@ -35,57 +35,19 @@ Rails.application.routes.draw do
 
   # Petición, sirve para cuando no se pone nada
   root :to => 'about#index'
-
   get 'about' => 'about#index'
-
   get 'about/index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get 'admin/provider/new'
+  post 'admin/provider/create'
+  get 'admin/provider/edit'
+  post 'admin/provider/update'
+  post 'admin/provider/destroy'
+  get 'admin/provider/show'
+  get 'admin/provider/show/:id' => 'admin/provider#show'
+  get 'admin/provider/index'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
