d8952e512f49dda59dbd6dce6900691624178e8f
Erwol <ernes.wo@gmail.com>
2016-06-04 18:10:28 +0200

    Test del foro

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,12 @@
 class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
-    user.send_password_reset if user
-    redirect_to root_url, :notice => "Se envió un email a la dirección indicada con las instrucciones para restaurar la contraseña."
+    if user
+      user.send_password_reset
+      redirect_to root_url, :notice => "Se envió un email a la dirección indicada con las instrucciones para restaurar la contraseña."
+    else
+      redirect_to root_url, :notice => "Este correo no está registrado en el sistema."
+    end
   end
 
   def edit
@@ -13,7 +17,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
     @user.update_attributes(params.require(:user).permit(:password, :password_confirmation))
 
-    if @user.password_reset_sent_at < 2.hours.ago
+    if @user.password_reset_sent_at < 5.minutes.ago
       redirect_to new_password_reset_path, :alert => "El token de reseteo ha caducado."
       # http://stackoverflow.com/questions/17568062/activemodelforbiddenattributeserror-in-passwordresetscontrollerupdate
     elsif @user.update_attributes(params.require(:user).permit(:password, :password_confirmation))
